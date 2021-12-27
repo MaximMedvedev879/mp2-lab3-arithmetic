e8f9147990f792cc91c41f7a2d37ec7590241c59
MaximMedvedev879 <76689189+MaximMedvedev879@users.noreply.github.com>
2021-12-27 18:49:17 +0300

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -1,9 +1,139 @@
-// объявление и реализация шаблонного стека
-// стек поддерживает операции: 
-// - вставка элемента, 
-// - извлечение элемента, 
-// - просмотр верхнего элемента (без удаления)
-// - проверка на пустоту, 
-// - получение количества элементов в стеке
-// - очистка стека
-// при вставке в полный стек должна перевыделяться память
+// РѕР±СЉСЏРІР»РµРЅРёРµ Рё СЂРµР°Р»РёР·Р°С†РёСЏ С€Р°Р±Р»РѕРЅРЅРѕРіРѕ СЃС‚РµРєР°
+// СЃС‚РµРє РїРѕРґРґРµСЂР¶РёРІР°РµС‚ РѕРїРµСЂР°С†РёРё: 
+// - РІСЃС‚Р°РІРєР° СЌР»РµРјРµРЅС‚Р°, 
+// - РёР·РІР»РµС‡РµРЅРёРµ СЌР»РµРјРµРЅС‚Р°, 
+// - РїСЂРѕСЃРјРѕС‚СЂ РІРµСЂС…РЅРµРіРѕ СЌР»РµРјРµРЅС‚Р° (Р±РµР· СѓРґР°Р»РµРЅРёСЏ)
+// - РїСЂРѕРІРµСЂРєР° РЅР° РїСѓСЃС‚РѕС‚Сѓ, 
+// - РїРѕР»СѓС‡РµРЅРёРµ РєРѕР»РёС‡РµСЃС‚РІР° СЌР»РµРјРµРЅС‚РѕРІ РІ СЃС‚РµРєРµ
+// - РѕС‡РёСЃС‚РєР° СЃС‚РµРєР°
+// РїСЂРё РІСЃС‚Р°РІРєРµ РІ РїРѕР»РЅС‹Р№ СЃС‚РµРє РґРѕР»Р¶РЅР° РїРµСЂРµРІС‹РґРµР»СЏС‚СЊСЃСЏ РїР°РјСЏС‚СЊ
+#ifndef __STACK_H__
+#define __STACK_H__
+#include <iostream>
+using namespace std;
+const int MAXSIZE = 1000000000;
+
+template <class ValType>
+class TStack
+{
+private:
+	ValType* stack;
+	int size;
+	int end; //РёРЅРґРµРєСЃ РїРѕСЃР»РµРґРЅРµРіРѕ СЌР»РµРјРµРЅС‚Р°
+
+public:
+	TStack(int n = MAXSIZE);
+	TStack(const TStack& v);
+	~TStack();
+	ValType Pop(); //РёР·РІР»РµС‡РµРЅРёРµ СЌР»РµРјРµРЅС‚Р°
+	ValType Get(); //РёР·РІР»РµС‡РµРЅРёРµ СЌР»РµРјРµРЅС‚Р° Р±РµР· СѓРґР°Р»РµРЅРёСЏ
+	void Push(double elem); //РІСЃС‚Р°РІРєР° СЌР»РµРјРµРЅС‚Р°
+	bool IsEmpty(); //РїСЂРѕРІРµСЂРєР° РЅР° РїСѓСЃС‚РѕС‚Сѓ
+	ValType View(); //РїСЂРѕСЃРјРѕС‚СЂ РІРµСЂС…РЅРµРіРѕ СЌР»РµРјРµРЅС‚Р°
+	int End(); //РїРѕР»СѓС‡РµРЅРёРµ РёРЅРґРµРєСЃР° РїРѕСЃР»РµРґРЅРµРіРѕ СЌР»РµРјРµРЅС‚Р°
+	int Size(); //РїРѕР»СѓС‡РµРЅРёРµ РєРѕР»РёС‡РµСЃС‚РІР° СЌР»РµРјРµРЅС‚РѕРІ РІ СЃС‚РµРєРµ
+	void Clear(); //РѕС‡РёСЃС‚РєР° СЃС‚РµРєР°
+};
+
+template <class ValType>
+TStack<ValType>::TStack(int n)
+{
+	if ((n <= 0) || (n > MAXSIZE)) throw "incorrect n";
+	else
+	{
+		stack = new ValType[n];
+		end = -1;
+		size = n;
+	}
+}
+
+template <class ValType>
+TStack<ValType>::TStack(const TStack<ValType>& v)
+{
+	size = v.size;
+	end = v.end;
+	stack = new ValType[size];
+	for (int i = 0; i < end; i++)
+		stack[i] = v.stack[i];
+}
+
+template <class ValType>
+TStack<ValType>::~TStack()
+{
+	delete[]stack;
+}
+template <class ValType>
+ValType TStack <ValType>::Pop()
+{
+	if ((*this).IsEmpty()) throw "Stack is empty";
+	else
+	{
+		ValType res;
+		res = stack[end];
+		end--;
+		return res;
+	}
+}
+template <class ValType>
+ValType TStack<ValType>::Get()
+{
+	if ((*this).IsEmpty()) throw "Stack is empty";
+	else
+		return stack[end];
+}
+
+template <class ValType>
+int TStack <ValType>::Size()
+{
+	return (end + 1);
+}
+
+template <class ValType>
+int TStack <ValType>::End()
+{
+	return end;
+}
+
+template <class ValType>
+void TStack<ValType>::Push(double elem)
+{
+	if (end == size)
+	{
+		ValType* temp = stack;
+		stack = new ValType[2*size];
+		for (int i = 0; i < size; i++)
+			stack[i] = temp[i];
+		delete[]temp;
+
+		size = 2 * size;
+	}
+	end++;
+	stack[end] = elem;
+}
+template <class ValType>
+void TStack<ValType>::Clear()
+{
+	end = -1;
+	size = 0;
+	delete[]stack;
+	stack = new ValType[size];
+}
+
+template <class ValType>
+bool TStack<ValType>::IsEmpty()
+{
+	if (end == -1)
+		return true;
+	else return false;
+
+}
+
+
+template <class ValType>
+ValType TStack <ValType>::View()
+{
+	if (IsEmpty()) throw "Stack is empty";
+	else
+		return stack[end];
+}
+#endif
