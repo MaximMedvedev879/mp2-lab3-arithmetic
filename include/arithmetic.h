0880be5ece517e454bed8a9761991ff6a718e0dd
MaximMedvedev879 <76689189+MaximMedvedev879@users.noreply.github.com>
2021-12-27 18:51:21 +0300

    Update arithmetic.h

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,39 @@
-// объявление функций и классов для вычисления арифметических выражений
+// РѕР±СЉСЏРІР»РµРЅРёРµ С„СѓРЅРєС†РёР№ Рё РєР»Р°СЃСЃРѕРІ РґР»СЏ РІС‹С‡РёСЃР»РµРЅРёСЏ Р°СЂРёС„РјРµС‚РёС‡РµСЃРєРёС… РІС‹СЂР°Р¶РµРЅРёР№
+#ifndef __ARITHMETIC_H__
+#define __ARITHMETIC_H__
+
+#include <iostream>
+#include <cctype>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include "stack.h"
+
+using namespace std;
+
+int DeterminationType(char s);
+void ProcessingUnaryMinus(char* s, char* res);
+bool ThereIsUnaryMinus(char* s);
+void ReplacementPointToCommaInDouble(char* s);
+
+bool CheckBrackets(char* s);
+bool CheckAmountOperands(char* s);
+bool CheckOperationsInRow(char* s);
+bool CheckPlaceDotsOrCommas(char* s);
+bool CheckCorrectnessOfSymbols(char* s);
+bool CheckAmountLettersaInVars(char* s);
+bool CheckVariableToDigit(char* s);
+bool CheckStr(char* s);
+
+int Prioritet(char s);
+bool IsSign(char s);
+bool IsOperation(char s);
+void FindVars(const char* s, int* res);
+bool CheckCorrecnessOfValuesOfVars(char* s);
+void InputValues(char* s, char* res);
+double GetNumber(char* s);
+
+void ConvertInPostfixNotation(const char* s, char* res);
+double EvaluationOfExpression(char* s);
+
+#endif
